c1e347543c542bf9a5fd8bad749e8b9532ddf8f3
tcaplan <tcaplan9@gmail.com>
2018-12-29 05:30:49 -0500

    Added AdModule From Old Android Game And Object Pooler From Old RTS Game

---
+++ AdModule.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdModule : MonoBehaviour 
+{
+    public enum AdRewardType
+    {
+        None,
+        ExtraLives,
+        DarkMatter
+    }
+
+    public enum AdErrorType
+    {
+        Generic,
+        Failed,
+        Unsupported
+    }
+
+    string[] m_ErrorMessages = new string[]
+    {
+        "There has been an error loading advertisement.",
+        "Check internet connection and restart application.",
+        "Advertisement is unsupported and failed to load."
+    };
+
+    [SerializeField]
+    private string m_AndroidGameId = "";
+
+    [SerializeField]
+    private bool m_TestModeEnabled = false;
+
+    [SerializeField]
+    private string m_RewardVideoString = "rewardedVideo";
+
+    float m_AdDisplayTimeOut = 5.0f;
+    float m_CurrentDisplayTimeOut = 0.0f;
+    float m_TimeOutInterval = 0.5f;
+
+    bool m_HasTimedOut = false;
+
+    ShowOptions m_ShowOptions;
+
+    TitleSceneHandler m_CurrentTitleSceneHandler;
+
+    void Start()
+    {
+        m_ShowOptions = new ShowOptions();
+    }
+
+    public void ShowAd(AdRewardType type, TitleSceneHandler handler)
+    {
+        m_CurrentTitleSceneHandler = handler;
+        if (Advertisement.isSupported)
+        {
+            switch (type)
+            {
+                case AdRewardType.ExtraLives:
+                    {
+                        StartCoroutine(ShowRewardAd_Lives());
+                        break;
+                    }
+                case AdRewardType.DarkMatter:
+                    {
+                        StartCoroutine(ShowRewardAd_DarkMatter());
+                        break;
+                    }
+                case AdRewardType.None:
+                    {
+                        break;
+                    }
+                default: break;
+            }
+        }
+
+        else DisplayAdError(AdErrorType.Unsupported);
+    }
+
+    private void DisplayAdError(AdErrorType type)
+    {
+        m_CurrentTitleSceneHandler.DisplayAdError(m_ErrorMessages[(int)type]);
+    }
+
+    private IEnumerator ShowRewardAd_Lives()
+    {
+        Advertisement.Initialize(m_AndroidGameId, m_TestModeEnabled);
+
+        while (!Advertisement.IsReady(m_RewardVideoString) && !m_HasTimedOut)
+        {
+            yield return new WaitForSecondsRealtime(m_TimeOutInterval);
+            if (QueryAdTimeOut())
+            {
+                RewardAdCallback_ExtraLives(ShowResult.Failed);
+                m_HasTimedOut = true;
+            }
+        }
+
+        if (!m_HasTimedOut)
+        {
+            m_ShowOptions.resultCallback = RewardAdCallback_ExtraLives;
+            Advertisement.Show(m_RewardVideoString, m_ShowOptions);
+        }
+
+        m_HasTimedOut = false;
+    }
+
+    private IEnumerator ShowRewardAd_DarkMatter()
+    {
+        Advertisement.Initialize(m_AndroidGameId, m_TestModeEnabled);
+
+        while (!Advertisement.IsReady(m_RewardVideoString) && !m_HasTimedOut)
+        {
+            yield return new WaitForSecondsRealtime(m_TimeOutInterval);
+            if (QueryAdTimeOut())
+            {
+                RewardAdCallback_DarkMatter(ShowResult.Failed);
+                m_HasTimedOut = true;
+            }
+        }
+
+        if (!m_HasTimedOut)
+        {
+            m_ShowOptions.resultCallback = RewardAdCallback_DarkMatter;
+            Advertisement.Show(m_RewardVideoString, m_ShowOptions);
+        }
+
+        m_HasTimedOut = false;
+    }
+
+    bool QueryAdTimeOut()
+    {
+        if (m_CurrentDisplayTimeOut < m_AdDisplayTimeOut)
+            m_CurrentDisplayTimeOut += m_TimeOutInterval;
+
+        if (m_CurrentDisplayTimeOut >= m_AdDisplayTimeOut)
+        {
+            m_CurrentDisplayTimeOut = 0;
+            return true;
+        }
+        
+        return false;
+    }
+
+    private void RewardAdCallback_ExtraLives(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Failed:
+                {
+                    DisplayAdError(AdErrorType.Failed);
+                    break;
+                }
+            case ShowResult.Finished:
+                {
+                    m_CurrentTitleSceneHandler.RewardPlayerForAd_ExtraLives();
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    Debug.Log("Reward Callback For Extra Lives: Skipped");
+                    break;
+                }
+            default: break;
+        }
+    }
+
+    private void RewardAdCallback_DarkMatter(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Failed:
+                {
+                    DisplayAdError(AdErrorType.Failed);
+                    break;
+                }
+            case ShowResult.Finished:
+                {
+                    m_CurrentTitleSceneHandler.RewardPlayerForAd_DarkMatter();
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    Debug.Log("Reward Callback For Dark Matter: Skipped");
+                    break;
+                }
+            default: break;
+        }
+    }
+}
---
+++ ObjectPoolHandler.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPoolable
+{
+    int ParentInstanceId { get; set; }
+
+    void OnExtraction();
+}
+
+#pragma warning disable
+public class ObjectPoolHandler : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> m_PrefabsToPool = new List<GameObject>();
+    [SerializeField]
+    List<int> m_ObjectPoolCounts = new List<int>();
+    [SerializeField]
+    bool m_DebugMode;
+
+    private Dictionary<int, Queue<GameObject>> 
+        m_ObjectPool = new Dictionary<int, Queue<GameObject>>();
+
+    private Dictionary<int, PooledObjectState>
+        m_ObjectStates = new Dictionary<int, PooledObjectState>();
+
+    Vector3 m_AlphaPosition = Vector3.down * 100f;
+
+    private void Awake()
+    {
+        if (m_PrefabsToPool.Count > 0)
+        {
+            InitializeObjectPools();
+        }
+    }
+
+    private void InitializeObjectPools()
+    {
+        for (int i = m_PrefabsToPool.Count - 1; i >= 0; i--)
+        {
+            if (!m_ObjectPool.ContainsKey(m_PrefabsToPool[i].GetInstanceID()))
+            {
+                if (m_PrefabsToPool[i].GetComponent<IPoolable>() != null)
+                {
+                    if(GameEngine.DebugMode && m_DebugMode)
+                        Debug.Log(string.Format("Object Pool Handler Creating Pool For Object ({0}[{1}])",
+                            m_PrefabsToPool[i], m_PrefabsToPool[i].GetInstanceID()));
+
+                    int id = m_PrefabsToPool[i].GetInstanceID();
+
+                    m_ObjectPool.Add(id, new Queue<GameObject>());
+                    m_ObjectStates.Add(id, new PooledObjectState(m_PrefabsToPool[i]));
+
+                    PopulateIndividualQueues(i, id);
+                }
+                else throw new UnityException(string.Format
+                    ("Object Pool Handler Attempting To Pool Non-Poolable Object ({0})!", m_PrefabsToPool[i]));
+            }
+
+            else throw new UnityException(string.Format
+                ("Object Pool Handler Attempting To Pool Duplicate Object Instances ({0})!", m_PrefabsToPool[i]));
+        }
+    }
+
+    private void PopulateIndividualQueues(int i, int id)
+    {
+        for (int j = m_ObjectPoolCounts[i] - 1; j >= 0; j--)
+        {
+            GameObject instance = Materialize(m_PrefabsToPool[i], m_AlphaPosition);
+
+            instance.GetComponent<IPoolable>().ParentInstanceId = id;
+            instance.SetActive(false);
+
+            m_ObjectPool[id].Enqueue(instance);
+        }
+
+        if (GameEngine.DebugMode && m_DebugMode)
+            Debug.Log(string.Format("Pool Count For {0}: {1}", id, m_ObjectPool[id].Count));
+    }
+
+    internal GameObject ExtractObject(GameObject prefab, Vector3 position)
+    {
+        int id = prefab.GetInstanceID();
+        if (m_ObjectPool.ContainsKey(id))
+        {
+            if (GameEngine.DebugMode && m_DebugMode)
+                Debug.Log(string.Format("Attempting To Extract {2} From {0} [Pool Count: {1}]", id, m_ObjectPool[id].Count, prefab.name));
+
+            if (m_ObjectPool[id].Count > 0)
+            {
+                GameObject deltaObject = m_ObjectPool[id].Dequeue();
+                PooledObjectState state = m_ObjectStates[id];
+
+                if (GameEngine.DebugMode && m_DebugMode)
+                    Debug.Log(string.Format("Pool[{0}] Object Count: ({1})", id, m_ObjectPool[id].Count));
+
+                deltaObject.GetComponent<IPoolable>().OnExtraction();
+
+                deltaObject.transform.localScale = state.OriginalScale;
+                deltaObject.transform.position = position;
+                deltaObject.SetActive(true);
+
+                return deltaObject;
+            }   return null;
+        }       throw new UnityException(string.Format("Object Pool Does Not Contain {0} With ID {1}", prefab, id));
+    }
+
+    internal void ReclaimObject(int id, GameObject instance)
+    {
+        instance.SetActive(false);
+        instance.transform.position = m_AlphaPosition;
+        instance.transform.parent = null;
+
+        if (GameEngine.DebugMode && m_DebugMode)
+            Debug.Log(string.Format("Reclaiming {0} For Object Pool {1}", instance.name, id));
+
+        if (!m_ObjectPool[id].Contains(instance))
+            m_ObjectPool[id].Enqueue(instance);
+
+        else
+            Debug.Log(string.Format("Pool[{1}] Already Contains {0}", instance.name, id));
+
+        if (GameEngine.DebugMode && m_DebugMode)
+            Debug.Log(string.Format("[Pool Count After Enqueue: {0}]", m_ObjectPool[id].Count));
+    }
+
+    internal void AssertPoolExists(GameObject prefab)
+    {
+        if (!m_ObjectPool.ContainsKey(prefab.GetInstanceID()))
+            throw new UnityException(string.Format("Object Pool Does Not Contain {0}", prefab));
+    }
+}
+
+public class PooledObjectState
+{
+    Vector3 m_OriginalScale;
+
+    public Vector3 OriginalScale
+    {
+        get { return m_OriginalScale; }
+    }
+
+    public PooledObjectState(GameObject objectRelative)
+    {
+        m_OriginalScale = objectRelative.transform.localScale;
+    }
+}
+
